4ea25229bffada2b08cfbaaa3442285d5e4f0e86
Garrett Mooney <4910020+GarrettMooney@users.noreply.github.com>
2020-09-14 01:36:24 -0400

    Add flake, black, and pre-commit to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
 
-base_packages = []
-
 test_packages = [
     "pytest>=5.4.3",
+    "black>=19.10b0",
+    "flake8>=3.8.3",
 ]
 
-util_packages = [
-    "jupyterlab>=2.2.0",
-]
+util_packages = ["jupyterlab>=2.2.0", "pre-commit>=2.6.0"]
 
 dev_packages = test_packages + util_packages
 
 setup(
     name="moonpy",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     extras_require={"dev": dev_packages, "test": test_packages},
 )
